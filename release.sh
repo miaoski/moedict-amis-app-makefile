b0bb53cea788dd9036278dfe28f9bf777724e12b
miaoski <lin.zhemin@gmail.com>
2015-12-26 16:03:31 +0800

    merge with sign.sh

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 [version]"
-	exit 1
-fi
-ver="$1"
-./zipalign -f 4 moedict-amis-${ver}-signed.apk moedict-amis-${ver}-aligned.apk
-echo "Don't forget to modify moedict-amis/apktool.yml"
-echo "Upload moedict-amis-${ver}-aligned.apk"
--- sign.sh
+++ sign.sh
@@ -7,4 +7,6 @@ ver=$1
 cp -v moedict-amis/assets/www/cordova.js.orig moedict-amis/assets/www/cordova.js
 apktool b -o moedict-amis-${ver}.apk moedict-amis
 java -jar SignApk/signapk.jar cert.x509.pem private.pk8 moedict-amis-${ver}.apk moedict-amis-${ver}-signed.apk
+./zipalign -f 4 moedict-amis-${ver}-signed.apk moedict-amis-${ver}-aligned.apk
+echo "Upload moedict-amis-${ver}-aligned.apk"
 ls -l *${ver}*.apk
