789775a5cdbb13e933220c770f63d3115b0fce1f
miaoski <lin.zhemin@gmail.com>
2015-12-26 16:25:53 +0800

    fix readme

--- sign.sh
+++ sign.sh
@@ -5,7 +5,7 @@ if [ $# -ne 1 ]; then
 fi
 ver=$1
 cp -v moedict-amis/assets/www/cordova.js.orig moedict-amis/assets/www/cordova.js
-apktool b -o moedict-amis-${ver}.apk moedict-amis
+apktool b -f -o moedict-amis-${ver}.apk moedict-amis
 java -jar SignApk/signapk.jar cert.x509.pem private.pk8 moedict-amis-${ver}.apk moedict-amis-${ver}-signed.apk
 ./zipalign -f 4 moedict-amis-${ver}-signed.apk moedict-amis-${ver}-aligned.apk
 echo "Upload moedict-amis-${ver}-aligned.apk"
