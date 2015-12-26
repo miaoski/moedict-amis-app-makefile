90ca2e9f0e8f9fccd9509d46b85c9458a80e2f8b
miaoski <lin.zhemin@gmail.com>
2015-12-26 16:00:43 +0800

    bugfix: I need more glucose

--- sign.sh
+++ sign.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
 	echo "Usage: $0 [version]"
+	exit 1
 fi
 ver=$1
 cp -v moedict-amis/assets/www/cordova.js.orig moedict-amis/assets/www/cordova.js
 apktool b -o moedict-amis-${ver}.apk moedict-amis
 java -jar SignApk/signapk.jar cert.x509.pem private.pk8 moedict-amis-${ver}.apk moedict-amis-${ver}-signed.apk
-ls -l *${ver}*
+ls -l *${ver}*.apk
