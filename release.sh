3aa04541cefd432306f35e03dc51038de580bf99
miaoski <lin.zhemin@gmail.com>
2015-12-26 16:01:05 +0800

    friendly notice

--- release.sh
+++ release.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ./zipalign 4 moedict-amis-${ver}-signed.apk moedict-amis-${ver}-aligned.apk
 echo "Don't forget to modify moedict-amis/apktool.yml"
+echo "Upload moedict-amis-${ver}-aligned.apk"
